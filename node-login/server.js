7f20f074e633a965afdf2fd968ab118111ebc39c
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-06-04 17:03:40 +0530

    Create server.js

---
+++ node-login/server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var options = {transport: ['websocket']};
+var  io  = require('socket.io')(options).listen(app.listen(process.env.PORT || 3000));
+
+
+app.get('/', function(req, res){
+     res.sendfile('index.html');
+    });
+
+ app.use(express.static(__dirname + '/'));
+
+var users=[];
+var idsnicks={};
+io.on('connection', function (socket) {
+
+  socket.on('login', function  (nick) {
+    users.push(nick);
+    socket.nick=nick;
+    idsnicks[nick]=socket.id;
+    io.emit('userlist', users);
+  })
+
+  socket.on('send', function  (data) {
+    if (io.sockets.connected[idsnicks[data.usr]]!==undefined) {
+    io.sockets.connected[idsnicks[data.usr]].emit('sendmsg', {msg:data.msg, usr:socket.nick});
+   }
+  })
+
+  socket.on('startchat', function  (data) {
+  if (io.sockets.connected[idsnicks[data]]!==undefined) {
+    io.sockets.connected[idsnicks[data]].emit('openchat', socket.nick);
+  }
+  })
+
+  socket.on('disconnect', function () {
+      console.log('disc');
+     users.splice( users.indexOf(socket.nick), 1 );
+     delete idsnicks[socket.nick];
+    io.emit('discon', {usr:socket.nick, list:users});
+
+
+      });
+
+  });
