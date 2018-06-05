3ccdbfe8f35c7bfe271732b4883d2f41083b2c50
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-06-05 14:13:48 +0530

    Update user.js

--- node-login/models/user.js
+++ node-login/models/user.js
@@ -3,18 +3,21 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const userSchema = mongoose.Schema({ 
 
 	name 			: String,
 	email			: {type: String, unique: true}, 
 	mobile			: String,
-	hashed_password	: String,
+	hashed_password 	: String,
 	created_at		: String,
-	temp_password	: String,
-	temp_password_time: String
+	temp_password   	: String,
+	temp_password_time      : String
 	
 });
+
+userSchema.plugin(AutoIncrement, {inc_field: 'user_id', disable_hooks: true});
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/node-login');
