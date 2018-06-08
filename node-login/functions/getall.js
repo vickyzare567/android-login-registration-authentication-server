7e79fdd3df2f71360d736e742af8cb8559829076
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-06-08 13:12:42 +0530

    Update getall.js

--- node-login/functions/getall.js
+++ node-login/functions/getall.js
@@ -3,7 +3,7 @@ var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  var dbo = db.db("mydb");
+  var dbo = db.db("node-login");
   dbo.collection("customers").find({}).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
