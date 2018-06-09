8964f5f28acf1d0cce895ff9205088a4fd1259b4
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-06-09 12:48:59 +0530

    Update messages.js

--- node-login/models/messages.js
+++ node-login/models/messages.js
@@ -5,16 +5,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
-const userSchema = mongoose.Schema({ 
+const messageSchema = mongoose.Schema({ 
 
-	name 			: String,
-	email			: {type: String, unique: true}, 
-	mobile			: String,
-	hashed_password 	: String,
-	created_at		: String,
-	temp_password   	: String,
-	temp_password_time      : String
-	
+	from 			: String,
+	to			: String,
+	time			: String,
+	status		 	: String,
+	delivered_time		: String	
 });
 
 userSchema.plugin(AutoIncrement, {inc_field: 'user_id', disable_hooks: true});
@@ -22,4 +19,4 @@ userSchema.plugin(AutoIncrement, {inc_field: 'user_id', disable_hooks: true});
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/node-login');
 
-module.exports = mongoose.model('user', userSchema); 
+module.exports = mongoose.model('messages', userSchema); 
