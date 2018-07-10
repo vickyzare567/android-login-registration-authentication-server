1b3f3a1d0ae1a05a64446f8f14da895d72d942df
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-07-10 19:28:23 +0530

    Update getallusers.js

--- node-login/functions/getallusers.js
+++ node-login/functions/getallusers.js
@@ -9,13 +9,13 @@ exports.getProfile = email =>
 	new Promise((resolve,reject) => {
 
 		MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("node-login");
-    res= dbo.collection("users").find({}).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    db.close();
-      });
+		if (err) throw err;
+		var dbo = db.db("node-login");
+		res= dbo.collection("users").find({}).toArray(function(err, result) {
+		if (err) throw err;
+		console.log(result);
+		db.close();
+		});
     });
 		
     .then(users => resolve(res))
