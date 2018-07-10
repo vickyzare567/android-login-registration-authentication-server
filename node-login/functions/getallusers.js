2a827eb907c267c76c019e6ee14d3d68698eecd8
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-07-10 20:07:11 +0530

    Update getallusers.js

--- node-login/functions/getallusers.js
+++ node-login/functions/getallusers.js
@@ -1,12 +1,24 @@
 'use strict';
 
 const user = require('../models/user');
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/node-login";
-var res;
-exports.getProfile = email => 
+
+exports.getProfile  => 
 	
-	new Promise((resolve,reject) => {
+new Promise((resolve,reject) => {
+
+		user.find({})
+
+		.then(users => resolve(users[0]))
+
+		.catch(err => reject({ status: 500, message: 'Internal Server Error !' }))
+
+	});	
+
+
+
+
+
+/* new Promise((resolve,reject) => {
 
 		MongoClient.connect(url, function(err, db) {
 		if (err) throw err;
@@ -16,10 +28,15 @@ exports.getProfile = email =>
 		console.log(result);
 		db.close();
 		});
-    });
-		
-    .then(users => resolve(res))
+			
+		.then(users => resolve(res))
 
 		.catch(err => reject({ status: 500, message: 'Internal Server Error !' }))
+    });
+		
+    
 
 	});
+*/
+
+
