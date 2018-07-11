52846652020d927dcf599c6e20d273de1a1cbfcc
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-07-11 19:53:03 +0530

    Update getallusers.js

--- node-login/functions/getallusers.js
+++ node-login/functions/getallusers.js
@@ -6,7 +6,7 @@ exports.getProfile = email =>
 	
 new Promise((resolve,reject) => {
 
-		 u= user.find({ }, { name: 1, email: 1, mobile : 1, created_at: 1, _id: 0 })
+		 u= user.find({email:""}, { name: 1, email: 1, mobile : 1, created_at: 1, _id: 0 })
 	
 		.then(users => resolve(users[0]))
 
