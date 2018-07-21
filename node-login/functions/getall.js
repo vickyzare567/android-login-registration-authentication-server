1cef80ba049fd0e2f82d17f7f896ba232c443169
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-07-21 15:04:04 +0530

    Update getall.js

--- node-login/functions/getall.js
+++ node-login/functions/getall.js
@@ -1,12 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/node-login";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("node-login");
-  dbo.collection("users").find({}).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    db.close();
-  });
+
+app.get('/', (req, res) => {
+   
+              MongoClient.connect(url, function(err, db) {
+              if (err) throw err;
+              var dbo = db.db("node-login");
+              dbo.collection("users").find({}).toArray(function(err, result) {
+                if (err) throw err;
+                console.log(result);
+                db.close();
+              });
+            });
+  
+  res.json(result);
+});
+
+
+
+
+
+app.listen(7769, () => {
+    console.log("Server is listening on port 7769");
 });
