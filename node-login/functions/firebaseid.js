71ffb1e82e8bae214369af0b2616c756b6b45417
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-07-31 17:08:39 +0530

    Update firebaseid.js

--- node-login/functions/firebaseid.js
+++ node-login/functions/firebaseid.js
@@ -1,1 +1,33 @@
 
+'use strict';
+
+const firebase = require('../models/firebase');
+const bcrypt = require('bcryptjs');
+
+exports.registerUser = (fid, did, email) => 
+
+	new Promise((resolve,reject) => {
+
+
+		const newFireid = new firebase({
+			fid: fid,
+			did: did,
+			email: email
+		});
+
+		newFireid.save()
+
+		.then(() => resolve({ status: 201, message: 'Fire Id Registered Sucessfully !' }))
+
+		.catch(err => {
+
+			if (err.code == 11000) {
+						
+				reject({ status: 409, message: 'Fire Id Already Registered !' });
+
+			} else {
+
+				reject({ status: 500, message: 'Internal Server Error !' });
+			}
+		});
+	});
