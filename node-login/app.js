ee3b35bca81c845c8279a23c93a27b7e885fe1f0
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-08-07 17:03:58 +0530

    Update app.js

--- node-login/app.js
+++ node-login/app.js
@@ -5,10 +5,15 @@ const app        = express();
 const bodyParser = require('body-parser');
 const logger 	   = require('morgan');
 const router 	   = express.Router();
-const port 	   = process.env.PORT || 8080;
+const port 	     = process.env.PORT || 8080;
+const fileUpload = require('express-fileupload');
+const imageapp   = express();
+const imageport  = process.env.PORT || 7769;
 
 app.use(bodyParser.json());
 app.use(logger('dev'));
+
+imageapp.use(fileUpload());
 
 require('./routes')(router);
 app.use('/api/v1', router);
@@ -16,3 +21,24 @@ app.use('/api/v1', router);
 app.listen(port);
 
 console.log(`App Runs on ${port}`);
+
+imageapp.post('/imageupload', function(req, res) {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+ 
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.sampleFile;
+ 
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv('/root/.UserImages/', function(err) {
+    if (err)
+      return res.status(500).send(err);
+ 
+    res.send('File uploaded!');
+  });
+});
+
+
+imageapp.listen(imageport);
+
+console.log("The Image Upload Server Running On : "+imageport);
