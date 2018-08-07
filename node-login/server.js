bda69887ebf43e1e29fd2f29fb6faf1ca3e56fef
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-08-07 14:44:35 +0530

    Update server.js

--- node-login/server.js
+++ node-login/server.js
@@ -104,7 +104,7 @@ function getFirebaseId(email,callback){
         var dbo = db.db("node-login");	
         // console.log('Email :- ',email);
         // ,{'fid' : true,'email':true}
-        dbo.collection("firebases").find({email:email},{'fid' : true,'email':true}).toArray(function(err, result) {
+        dbo.collection("user").find({email:email},{'firebase_id' : true,'email':true}).toArray(function(err, result) {
         if (err){ throw err; console.log(err); }
             firerecords=result;
         callback(firerecords);
