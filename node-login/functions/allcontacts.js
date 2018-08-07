ec0f48d4f70be17a94f123b795ba90ae67e8fb63
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-08-07 16:35:24 +0530

    Update allcontacts.js

--- node-login/functions/allcontacts.js
+++ node-login/functions/allcontacts.js
@@ -8,11 +8,8 @@ exports.getAllContacts = email =>
 	
 	new Promise((resolve,reject) => {
 
-		user.find({},{'name' : true, 'email':true, 'mobile':true}).toArray(function(err, result) {
-        	if (err) throw err;
-            		records=result;
-			console.log(records);
-		})
+		user.find({},{'name' : true, 'email':true, 'mobile':true})
+	
 		.then(records => resolve(records))
 
 		.catch(err => reject({ status: 500, message: 'Internal Server Error !' }))
