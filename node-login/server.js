ceec9437cdcf85ac745909a3832c8b17573a9993
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-09-03 17:33:54 +0530

    Update server.js

--- node-login/server.js
+++ node-login/server.js
@@ -157,7 +157,8 @@ function setOfflineStatus(email,callback){
 
 
 
-function saveMessage(from_email,to_email,message, callback){
+function saveMessage(from_email,to_email,message){
+	new Promise((resolve,reject) => {
 		const newMessage = new messageModel({
 			from_email : from_email,
 			to_email   : to_email,
@@ -171,17 +172,18 @@ function saveMessage(from_email,to_email,message, callback){
 
 		newMessage.save()
 
-		.then(() => callback({ status: 201, message: 'User Registered Sucessfully !' }))
+		.then(() => resolve({ status: 201, message: 'User Registered Sucessfully !' }))
 
 		.catch(err => {
 
 			if (err.code == 11000) {
 						
-				callback({ status: 409, message: 'User Already Registered !' });
+				reject({ status: 409, message: 'User Already Registered !' });
 
 			} else {
 
-				callback({ status: 500, message: 'Internal Server Error !' });
+				reject({ status: 500, message: 'Internal Server Error !' });
 			}
 		});
+	});
 }
