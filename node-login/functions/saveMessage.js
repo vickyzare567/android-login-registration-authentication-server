4263f37321ea3f76759ae470af5e6587a7a9aac6
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-09-03 17:40:06 +0530

    Update saveMessage.js

--- node-login/functions/saveMessage.js
+++ node-login/functions/saveMessage.js
@@ -1,1 +1,34 @@
+'use strict';
+const messageModel = require('./models/message');
+
+exports.save = (from_email, to_email, message ) => 
+
+	new Promise((resolve,reject) => {
+		const newMessage = new messageModel({
+			from_email : from_email,
+			to_email   : to_email,
+			message    : message,
+			time	   : new Date(),
+			has_image  : 'NONE',
+			image_url  : 'NONE',
+			isreaded   : 'FALSE',
+			isdelivered: 'FALSE'
+		});
+
+		newMessage.save()
+
+		.then(() => resolve({ status: 201, message: 'User Registered Sucessfully !' }))
+
+		.catch(err => {
+
+			if (err.code == 11000) {
+						
+				reject({ status: 409, message: 'User Already Registered !' });
+
+			} else {
+
+				reject({ status: 500, message: 'Internal Server Error !' });
+			}
+		});
+	});
 
