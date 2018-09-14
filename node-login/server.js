66d0c078f99b313a36211e874dee5233d66059bb
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-09-14 19:48:38 +0530

    Update server.js

--- node-login/server.js
+++ node-login/server.js
@@ -128,12 +128,11 @@ io.on('connection', function (socket) {
 socket.on('sendImage', function  (imageData) {
     console.log(imageData.from_user);
     if (io.sockets.connected[idsnicks[imageData.to_usr]]!==undefined) {
-		 if (imageData.image) {
-			//var img = new Image();
+		 	//var img = new Image();
 			//img.src = 'data:image/jpeg;base64,' + imageData.buffer;
 			//ctx.drawImage(img, 0, 0);
+			 console.log("Emmiting now ... ");
 			io.sockets.connected[idsnicks[imageData.to_usr]].emit('ImageMessage', { image: true, from_user: imageData.from_user, to_user: imageData.to_user, buffer: imageData.bufffer });
-		  }
     }else{
 	
 	}
