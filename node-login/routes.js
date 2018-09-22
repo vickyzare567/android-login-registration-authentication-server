bd221e96b673292a8d74a9eb6d03909a09491d7c
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-09-22 16:40:50 +0530

    Update routes.js

--- node-login/routes.js
+++ node-login/routes.js
@@ -72,7 +72,10 @@ module.exports = router => {
 				res.status(result.status).json({ message: result.message })
 			})
 
-			.catch(err => res.status(err.status).json({ message: err.message }));
+			.catch({
+				console.log(err);
+				err => res.status(err.status).json({ message: err.message });
+			});
 		}
 	});
 	
