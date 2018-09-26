d17394e71576922c251fd6f4308484aa0642b53e
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-09-26 19:07:28 +0530

    Update and rename freindsList.js to freindList.js

--- node-login/models/freindList.js
+++ node-login/models/freindList.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
-const friendslistSchema = mongoose.Schema({ 
+const friendlistSchema = mongoose.Schema({ 
 
 	  user_email 	        : String,
 	  with_contact_email    : String,	
@@ -16,9 +16,9 @@ const friendslistSchema = mongoose.Schema({
     
 });
 
-friendslistSchema.plugin(AutoIncrement, {inc_field: 'friends_id', disable_hooks: true});
+friendlistSchema.plugin(AutoIncrement, {inc_field: 'friends_id', disable_hooks: true});
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/node-login');
 
-module.exports = mongoose.model('freindList', friendslistSchema); 
+module.exports = mongoose.model('freindList', friendlistSchema); 
