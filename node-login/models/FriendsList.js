9d65ef17fbcb7dc1cdb46a2fb291d81eb2630923
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-09-26 16:49:51 +0530

    Create FriendsList.js

---
+++ node-login/models/FriendsList.js
@@ -0,0 +1,22 @@
+
+'use strict';
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const friendslistSchema = mongoose.Schema({ 
+
+	  user_email 	          : String,
+	  with_contact_email    : String,
+	  time                  : String,
+    
+});
+
+friendslistSchema.plugin(AutoIncrement, {inc_field: 'friends_id', disable_hooks: true});
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/node-login');
+
+module.exports = mongoose.model('friendslist', friendslistSchema); 
