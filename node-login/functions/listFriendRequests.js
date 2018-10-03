1ad305256b8c75d23007bc355cd8d89a4a22b4aa
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-10-03 19:12:22 +0530

    Update listFriendRequests.js

--- node-login/functions/listFriendRequests.js
+++ node-login/functions/listFriendRequests.js
@@ -3,11 +3,11 @@
 const friendList = require('../models/freindList');
 var records = [];
 
-exports.getFriendRequestsList = user_email => 
+exports.getFriendRequestsList = with_contact_email => 
 	
 	new Promise((resolve,reject) => {
 
-		friendList.find({user_email: user_email, request_status : 'UNAPPROVED' },{'user_email' : true, 'with_contact_email':true, 'time' : true , 'request_status':true})
+		friendList.find({with_contact_email: with_contact_email, request_status : 'UNAPPROVED' },{'user_email' : true, 'user_email':true, 'time' : true , 'request_status':true})
 	
 		.then(records => resolve(records))
 
