504ee1ae944c6d415322c26c715465d5ed4869f2
VICKY ZARE <31892952+vickyzare567@users.noreply.github.com>
2018-10-03 19:16:46 +0530

    Update listPendingRequests.js

--- node-login/functions/listPendingRequests.js
+++ node-login/functions/listPendingRequests.js
@@ -1,1 +1,17 @@
 
+'use strict';
+
+const friendList = require('../models/freindList');
+var records = [];
+
+exports.getPendingRequestsList = user_email => 
+	
+	new Promise((resolve,reject) => {
+
+		friendList.find({user_email: user_email, request_status : 'UNAPPROVED' },{'with_contact_email' : true, 'user_email':true, 'time' : true , 'request_status':true})
+	
+		.then(records => resolve(records))
+
+		.catch(err => reject({ status: 500, message: 'Internal Server Error !' }))
+
+	});
